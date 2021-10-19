9802b83f657804e9c7b6f4e3a13545d4d41e0354
chrisvsanchez <chrisvsanchez@gmail.com>
2021-10-19 16:10:55 -0400

    updated files

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router";
-
-function App() {
-  return <div>hello</div>;
-}
-
-export default App;
---
+++ src/components/UploadPosts.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function UploadPost() {
+  return <div>Post Upload Page</div>;
+}
+export default UploadPost;
