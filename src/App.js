99aae01c5a9a4cca6957cc0e8be6a6eefaa6f318
chrisvsanchez <chrisvsanchez@gmail.com>
2021-10-21 13:49:24 -0400

    created routes on app.js, installed router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import UploadPosts from "./components/UploadPosts";
 import Posts from "./containers/Posts";
 
 function App() {
-  return <div>hello friends</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/upload" component={UploadPosts} />
+          <Route path="/posts" component={Posts} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
